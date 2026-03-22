282d8890d752e2d2480bae959b9bdea0952cad9e
Daniel Barton <danielb@purpleflaghosting.com>
2026-03-22 20:51:31 +0800

    MariaDB: Mark 10.5 as EOL

--- mariadb/entrypoint.sh
+++ mariadb/entrypoint.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 cd /home/container || { echo "Could not change to /home/container directory! Exiting."; exit 1; }
 
+
+# Check for end-of-life marker
+if [ "${YOLK_EOL_NAG_WARNING+x}" ]; then
+	echo "======================================================================"
+	echo "DEPRECATION WARNING:"
+	echo "This version of MariaDB has been marked as end-of-life."
+	echo "Please migrate to a supported version as soon as possible to ensure"
+	echo "continued security updates and support."
+	echo "This image will be removed/disabled in the near future."
+	echo "======================================================================"
+
+	echo "Execution will continue in ${YOLK_EOL_NAG_DELAY:-10} seconds..."
+	sleep "${YOLK_EOL_NAG_DELAY:-10}"
+fi
+
+
 # Make internal Docker IP address available to processes.
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
